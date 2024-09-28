c2d077bef5747032882da57912a1cfb49bd198b6
w59w <kumiwa5@icloud.com>
2024-09-28 20:45:33 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+from aiogram import Bot, Dispatcher
+from decouple import config
 
+token=config("TOKEN")
+bot=Bot(token=token)
+dp = Dispatcher(bot=bot)
+
+
+STAFF_IDS = list(map(int, config('STAFF_IDS').split(',')))
