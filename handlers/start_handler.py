9562ab9a5b9e5d39c335735b152c55a9bdcfc5ef
w59w <kumiwa5@icloud.com>
2024-09-28 20:40:46 +0600

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,1 +1,10 @@
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
 
+
+async def cmd_start(message: types.Message):
+    await message.answer("Добро пожаловать! Используйте команды для взаимодействия с ботом.")
+
+
+def register_start_handler(dp: Dispatcher):
+    dp.register_message_handler(cmd_start, commands=['start'])
