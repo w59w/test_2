c10b5843b7d12fb24726ba9c5849b587e94aa0bb
w59w <kumiwa5@icloud.com>
2024-09-28 20:40:16 +0600

    Update info_handler.py

--- handlers/info_handler.py
+++ handlers/info_handler.py
@@ -1,1 +1,19 @@
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
 
+
+async def cmd_info(message: types.Message):
+    info_text = (
+        "Это бот для управления продуктами и заказами.\n"
+        "Команды:\n"
+        "/start - Начать взаимодействие с ботом\n"
+        "/info - Получить информацию о боте\n"
+        "/order - Сделать заказ\n"
+        "/add_product - Добавить новый продукт (доступно только для сотрудников)\n"
+        "/products - Просмотреть все товары"
+    )
+    await message.answer(info_text)
+
+
+def register_info_handler(dp: Dispatcher):
+    dp.register_message_handler(cmd_info, commands=['info'])
