b1141288327cfed5d33bcdb72a9443dd8c0f9a58
w59w <kumiwa5@icloud.com>
2024-09-28 20:39:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from aiogram import executor
+from config import dp 
+from handlers.fsm_order import register_order_handlers  
+from handlers.fsm_product import register_product_handlers 
+from handlers.start_handler import register_start_handler  
+from handlers.info_handler import register_info_handler  
 
+
+register_order_handlers(dp)
+register_product_handlers(dp)  
+register_start_handler(dp)  
+register_info_handler(dp)  
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
